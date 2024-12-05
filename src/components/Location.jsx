7678586f9dc8065d5a27401f13809fb5fd8d3eaf
Leshik1305 <realleshik1305@gmail.com>
2024-12-05 21:54:38 +0300

    Update Location.jsx

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import { fetchLocations, fetchCharacters } from "../api";
-import "./Location.css"
+import "./Rick.css"
+import { Collapse } from "./presentational/Collapse";
+
 
 export const Location = () =>{
     const [locations, setLocations] = useState([]);
     const [charactersByLocations, setCharactersByLocations] = useState({});
+    const [isLoadingByLocations, setIsLoadingByLocations] = useState({});
 
 
     useEffect(() => {
@@ -19,16 +22,34 @@ export const Location = () =>{
             const id = resident.split("/").pop();
             return id
         });
+
+        setIsLoadingByLocations({ ...isLoadingByLocations, [location.id]: true });
         fetchCharacters(ids).then((data) => {
-            console.log(data);
-            setCharactersByLocations({ ...charactersByLocations, [location.id]: data });
-      });
+            const characters = Array.isArray(data) ? data : [data];
+            setCharactersByLocations({ ...charactersByLocations, [location.id]: characters });
+            setIsLoadingByLocations({ ...isLoadingByLocations, [location.id]: false });
+        });
     }
+
+    const getStatusClass = (status) => {
+        switch (status) {
+          case "Alive":
+            return "character-alive";
+          case "Dead":
+            return "character-dead";
+          default:
+            return "character-unknown";
+        }
+      };
     
 
 
     return (
         <div>
+            <Collapse
+        className="buttons"
+        title = "LOCATIONS"
+        content =
             {locations.map((location) => {
                 return (
                     <div 
@@ -37,11 +58,27 @@ export const Location = () =>{
                         onClick = {() => handleLocationClick(location)}
                     >
                         <h3>{location.id + ": " + location.name}</h3>
-                        <div>
+                        <div className = "characters-container">
+                            {isLoadingByLocations[location.id] && (
+                                <div className="loading">Загрузка...</div>
+                            )}
                             {charactersByLocations[location.id]?.map((character) => {
                                 return(
-                                    <div key={location.id + ":" + character}>
-                                        {character.name}</div>
+                                    <div 
+                                        key={location.id + ":" + character.id} 
+                                        className={"character " + getStatusClass(character.status)}
+                                    >
+                                        <div className="character-left">
+                                            <img src={character.image} alt={character.name} />
+                                        </div>
+                                        <div className="character-right">
+                                            <h2>{character.name}</h2>
+                                            <div>Вид: {character.species}</div>
+                                            <div>Пол: {character.gender}</div>
+                                            
+                                            
+                                        </div>
+                                    </div>
                             )
 
                         })}
@@ -49,6 +86,7 @@ export const Location = () =>{
                     </div>
                 )
             })}
+            />
         </div>
     )
 }
