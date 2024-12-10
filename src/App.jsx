d0fb6a4a697628d693c9904ac04e773bf991774f
Leshik1305 <realleshik1305@gmail.com>
2024-12-10 22:24:04 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { Rick } from "./components/Rick";
 import { EpisodeList } from "./components/presentational/EpisodeList";
-import { Location } from "./components/Location";
 import { LocationList } from "./components/presentational/LocationList";
 
 function App() {
